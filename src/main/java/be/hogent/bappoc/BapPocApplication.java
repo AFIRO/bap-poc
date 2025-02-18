b8f3c336f012de7b482397cdeeb93b7234349c10
Andreeas Firoiu <74510849+AFIRO@users.noreply.github.com>
2025-02-18 16:46:56 +0100

    initial commit

---
+++ src/main/java/be/hogent/bappoc/BapPocApplication.java
@@ -0,0 +1,13 @@
+package be.hogent.bappoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BapPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/hogent/bappoc/BapPocApplicationTests.java
@@ -0,0 +1,13 @@
+package be.hogent.bappoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BapPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
