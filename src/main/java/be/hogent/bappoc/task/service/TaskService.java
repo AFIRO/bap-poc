60735b56e227cd0a6f45df823f401e31092edfb6
Andreeas Firoiu <74510849+AFIRO@users.noreply.github.com>
2025-02-25 13:00:53 +0100

    Workload balancing

--- src/main/java/be/hogent/bappoc/task/service/TaskService.java
+++ src/main/java/be/hogent/bappoc/task/service/TaskService.java
@@ -1,9 +1,12 @@
 package be.hogent.bappoc.task.service;
 
+import be.hogent.bappoc.shared.Employee;
+import be.hogent.bappoc.shared.EmployeeRepository;
 import be.hogent.bappoc.task.TaskMapper;
 import be.hogent.bappoc.task.dto.TaskInputDto;
 import be.hogent.bappoc.task.dto.TaskOutputDto;
 import be.hogent.bappoc.task.entity.Task;
+import be.hogent.bappoc.task.entity.TaskStatus;
 import be.hogent.bappoc.task.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -15,12 +18,13 @@ import java.util.List;
 @RequiredArgsConstructor
 @Log4j2
 public class TaskService {
-    private final TaskRepository repository;
+    private final TaskRepository taskRepository;
     private final TaskMapper mapper;
+    private final EmployeeRepository employeeRepository;
 
     public List<TaskOutputDto> getAll() {
         log.info("Getting data from repo");
-        List<Task> data = repository.findAll();
+        List<Task> data = taskRepository.findAll();
         return data
                 .stream()
                 .map(mapper::toOutputDto)
@@ -30,35 +34,44 @@ public class TaskService {
 
     public TaskOutputDto getByTaskInstanceReference(String taskInstanceReference) {
         log.info("Getting data from repo");
-        Task data = repository.getTaskByTaskInstanceReference(taskInstanceReference).orElseThrow();
+        Task data = taskRepository.getTaskByTaskInstanceReference(taskInstanceReference).orElseThrow();
         return mapper.toOutputDto(data);
     }
 
     public TaskOutputDto getByExecutorReference(String executorReference) {
         log.info("Getting data from repo");
-        Task data = repository.getTaskByExecutorReference(executorReference).orElseThrow();
+        Task data = taskRepository.getTaskByExecutorReference(executorReference).orElseThrow();
         return mapper.toOutputDto(data);
     }
 
     public TaskOutputDto getByInitiatorReference(String initiatorReference) {
         log.info("Getting data from repo");
-        Task data = repository.getTaskByInitiatorReference(initiatorReference).orElseThrow();
+        Task data = taskRepository.getTaskByInitiatorReference(initiatorReference).orElseThrow();
         return mapper.toOutputDto(data);
     }
 
     public void persistNewTask(Task createdTask ) {
-        repository.save(createdTask);
+        taskRepository.save(createdTask);
     }
 
     public void processTaskData(TaskInputDto data) {
-        if (!repository.existsByTaskInstanceReference(data.getTaskInstanceReference())) {
+        if (!taskRepository.existsByTaskInstanceReference(data.getTaskInstanceReference())) {
             log.error("Task with Instance Reference {} does not exist.", data.getTaskInstanceReference());
             throw new IllegalArgumentException("Task does not exist.");
         }
 
         log.info("Updating data for task {}",data.getTaskInstanceReference());
-        Task dataToUpdate = repository.getTaskByTaskInstanceReference(data.getTaskInstanceReference()).orElseThrow();
+        Task dataToUpdate = taskRepository.getTaskByTaskInstanceReference(data.getTaskInstanceReference()).orElseThrow();
         dataToUpdate = mapper.updateData(dataToUpdate,data);
-        repository.save(dataToUpdate);
+        taskRepository.save(dataToUpdate);
+        updateEmployeeWorkload(dataToUpdate);
+    }
+
+    private void updateEmployeeWorkload(Task data) {
+        if (data.getStatus() == TaskStatus.SUCCESS || data.getStatus() == TaskStatus.FAIL){
+            Employee employeeToUpdate = employeeRepository.findById(data.getExecutorReference()).orElseThrow();
+            employeeToUpdate.setNumberOfTasks((employeeToUpdate.getNumberOfTasks()-1));
+            employeeRepository.save(employeeToUpdate);
+        }
     }
 }
